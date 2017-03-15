1f20a73e78ba9e705ae9b1de62958dbe1d331839
Doubleshot1122 <skibo1122@yahoo.com>
2017-03-14 21:48:06 -0700

    initial push

---
+++ main.js
@@ -0,0 +1,27 @@
+console.log("main.js loaded");
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameSpace', {
+    preload: preload,
+    create: create,
+    update: update
+});
+
+function preload() {
+    // game.load.image('dungeon', 'images/dungeon.png');
+}
+
+//global scope variables
+// var player;
+
+function create() {
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+  // map = game.add.sprite(0, 0, 'map');
+
+
+  //game controls
+  cursors = game.input.keyboard.createCursorKeys();
+}
+
+function update() {
+
+}
